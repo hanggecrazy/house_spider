39b641a0b9d6347bd6d9b41da1e813549a7ada29
hanggecrazy <hanggecrazy@gmail.com>
2017-06-01 11:21:56 +0800

    Update spider_lj.py

--- spider_lj.py
+++ spider_lj.py
@@ -5,40 +5,33 @@ import common
 
 class spider_lj:
 
-    '''
-    获取楼盘标题
-    '''
+    #获取楼盘标题
     def getTitle(self, row):
-        """获取节点下html标签中div属性class=nlcd_name的元素html内容"""
+        #获取节点下html标签中div属性class=nlcd_name的元素html内容
         return row.find('h2').find('a').get_text()
     
-    '''
-    获取楼盘地理位置信息
-    '''
+
+    #获取楼盘地理位置信息
     def getAddr(self, row):
-        """获取节点下html标签中div属性class=address的元素html内容"""
+        #获取节点下html标签中div属性class=address的元素html内容
         return row.find('div', class_ = 'where').find('span').get_text()
     
-    '''
-    获取楼盘价格
-    '''
+    #获取楼盘价格
     def getPrice(self, row):
         price = ""
-        """获取节点下html标签中div属性class=nhouse_price的元素html内容"""
+        #获取节点下html标签中div属性class=nhouse_price的元素html内容
         price_div = row.find('div', class_ = 'average')
     
-        """判断div是否存在内容，存在则获取div下的span标签的文本内容"""        
+        #判断div是否存在内容，存在则获取div下的span标签的文本内容
         if price_div is not None :
             tmp = price_div.find('span').get_text()
             if tmp is not None  :
                 price = tmp 
         return price
     
-    '''
-    获取房源状态
-    '''
+    #获取房源状态
     def getType(self, row):
-        """获取房屋类型（1居／2居／3居....大小xxx-xx）"""
+        #获取房屋类型（1居／2居／3居....大小xxx-xx）
         types = ''
         type_div = row.find('div', class_ = 'type').find_all('span')
         if type_div is not None :
@@ -47,12 +40,10 @@ class spider_lj:
     
         return types
     
-    '''
-    获取房源面积大小
-    '''
+    #获取房源面积大小
     def getArea(self, row):
         area = ''
-        """获取房屋大小"""
+        #获取房屋大小
         area_div = row.find('div', class_ = 'area')
         if area_div is not None :
             area = area_div.get_text().replace(' ', '')
@@ -63,11 +54,9 @@ class spider_lj:
                 
         return area
     
-    '''
-    获取楼盘链接地址
-    '''
+    #获取楼盘链接地址
     def getUrl(self, row):
-        """获取节点下html标签中div属性class=nlcd_name的元素html内容"""
+        #获取节点下html标签中div属性class=nlcd_name的元素html内容
         return row.find('h2').find('a').get('href')
     
     
@@ -78,7 +67,7 @@ class spider_lj:
         sc = common.spider_common(url, cookie, field, max_pg, city)
         sc.analysis(cname, 'lj', 'info-panel', 'utf-8')
 
-"""主函数入口""" 
+#主函数入口
 if __name__ == '__main__': 
     city = 'bj'
     max_pg = 18
