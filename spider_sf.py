96062fe4e45dea959d1262df1423cf3d09695e84
hanggecrazy <hanggecrazy@gmail.com>
2017-06-01 11:13:43 +0800

    Update spider_sf.py

--- spider_sf.py
+++ spider_sf.py
@@ -3,12 +3,17 @@
 import os
 import common
 class spider_sf:
-
+    
+    '''
+    获取楼盘信息
+    '''
     def getTitle(self, row):
         """获取节点下html标签中div属性class=nlcd_name的元素html内容"""
         return row.find('div', class_ = "nlcd_name").find('a').get_text()
     
-    
+    '''
+    获取地址位置信息
+    '''
     def getAddr(self, row):
         """获取节点下html标签中div属性class=address的元素下的a标签下的span标签下的html内容"""
         addr_div = row.find('div', class_ = 'address').find('a')
@@ -23,9 +28,10 @@ class spider_sf:
     
         return addr
     
-    
+    '''
+    获取楼盘价格
+    '''
     def getPrice(self, row):
-        #print(row)
         """获取节点下html标签中div属性class=nhouse_price的元素html内容"""
         price_div = row.find('div', class_ = 'nhouse_price')
         
@@ -40,22 +46,32 @@ class spider_sf:
             price = row.find('div', class_ = 'kanzx').find('h3').find('a').get_text()
         else:
             price = row.find('div', class_ = 'kanesf').get_text()
+            
         return price
     
+    '''
+    获取房源类型
+    '''
     def getType(self, row):
         """获取房屋类型（1居／2居／3居....大小xxx-xx）"""
         return row.find('div', class_ = 'house_type').get_text()
     
+    '''
+    获取房源状态信息
+    '''
     def getStatus(self, row):
         """获取房屋状态，待售、售罄、开盘"""
         return row.find('div', class_ = 'fangyuan').find('span').get_text()
     
+    '''
+    获取房源链接
+    '''
     def getUrl(self, row):
         """获取节点下html标签中div属性class=nlcd_name的元素html内容"""
         return row.find('div', class_ = 'nlcd_name').find('a').get('href')
     
     
-    def mains(self, field, city, max_pg):
+    def main(self, field, city, max_pg):
         url = 'http://newhouse' + ('' if city == 'bj' else '.' + city)  + '.fang.com/house/s/b9' 
     
         cname = os.path.basename(__file__).split('.')[0]
@@ -64,12 +80,10 @@ class spider_sf:
         sc.analysis(cname, 'sf', 'nlc_details', 'gbk')
 
 """主函数入口""" 
-
 if __name__ == "__main__": 
 
     city = 'cd'
     max_pg = 101
-    
     field = [
         ["title", u"标题"], 
         ["price", u"价格"], 
@@ -79,4 +93,4 @@ if __name__ == "__main__":
         ["url", u"详情地址"]
     ] 
     
-    spider_sf().mains(field ,city, max_pg)
+    spider_sf().main(field ,city, max_pg)
